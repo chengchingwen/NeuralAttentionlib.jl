faa0df99d8ba19137e70ce757d5d1d401abc42ba
chengchingwen <adgjl5645@hotmail.com>
2021-08-07 03:31:25 +0800

    refine api type

--- src/functional/attention.jl
+++ src/functional/attention.jl
@@ -1,8 +1,8 @@
-@inline function generic_qkv_attention(mixingf, scoref, q::AbstractArray, k::AbstractArray, v::AbstractArray, args...)
+@inline function generic_qkv_attention(mixingf, scoref, q, k, v, args...)
     return unwrap_collapse(mixing(mixingf, v, scoref, q, k, args...))
 end
 
-@inline function generic_multihead_qkv_attention(mixingf, scoref, head::Integer, q::AbstractArray, k::AbstractArray, v::AbstractArray, args...)
+@inline function generic_multihead_qkv_attention(mixingf, scoref, head, q, k, v, args...)
     hq = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, q), static(2), static(ndims(q)))
     hk = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, k), static(2), static(ndims(k)))
     hv = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, v), static(2), static(ndims(v)))
@@ -13,11 +13,11 @@ end
 Base.@doc raw"""
 equivalent to `generic_qkv_attention(weighted_sum_mixing, normalized_score(NNlib.softmax) $ masked_score(mask) $ scaled_dot_product_score, q, k, v)`
 """
-@inline naive_qkv_attention(q::AbstractArray, k::AbstractArray, v::AbstractArray, mask=nothing) = unwrap_collapse(weighted_sum_mixing(
+@inline naive_qkv_attention(q, k, v, mask=nothing) = unwrap_collapse(weighted_sum_mixing(
     normalized_score(NNlib.softmax, masked_score, mask, scaled_dot_product_score, q, k),
     v))
 
-@inline function multihead_qkv_attention(head::Integer, q::AbstractArray, k::AbstractArray, v::AbstractArray, mask=nothing)
+@inline function multihead_qkv_attention(head, q, k, v, mask=nothing)
     hq = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, q), static(2), static(ndims(q)))
     hk = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, k), static(2), static(ndims(k)))
     hv = CollapsedDimArray((move_head_dim_out ∘ split_head)(head, v), static(2), static(ndims(v)))
@@ -28,5 +28,5 @@ end
 
 generic_attention(mixingf, scoref, q::AbstractArray, k::AbstractArray, v::AbstractArray, args...) = generic_qkv_attention(mixingf, scoref, q, k, v)
 generic_attention(mixingf, scoref, head::Integer, q::AbstractArray, k::AbstractArray, v::AbstractArray, args...) = generic_multihead_qkv_attention(mixingf, scoref, head, q, k, v)
-attention(head, q, k, v, mask=nothing) = multihead_qkv_attention(head, q, k, v, mask)
-attention(q, k, v, mask=nothing) = naive_qkv_attention(q, k, v, mask)
+attention(head::Integer, q::AbstractArray, k::AbstractArray, v::AbstractArray, mask=nothing) = multihead_qkv_attention(head, q, k, v, mask)
+attention(q::AbstractArray, k::AbstractArray, v::AbstractArray, mask=nothing) = naive_qkv_attention(q, k, v, mask)
