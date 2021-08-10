0a88f4f81205e078abfa3d9dad6cbd23893f4328
chengchingwen <adgjl5645@hotmail.com>
2021-08-10 08:22:50 +0800

    fix gpu and add BatchedMask

--- src/mask_new.jl
+++ src/mask_new.jl
@@ -1,3 +1,4 @@
+using Static
 include("./indexer.jl")
 
 """
@@ -26,30 +27,29 @@ Base.getindex(m::MaskIndexer, I...) = getmask_at(m, I...)
 
 GetIndexer(m::AbstractDatalessMask) = m
 
-getmask_at(m::AbstractAttenMask, i::CartesianIndex) = getmask_at(m, Tuple(i)...)
-getmask_at(m::MaskIndexer, i::CartesianIndex) = getmask_at(m, Tuple(i)...)
+getmask_at(m::AbstractAttenMask, i::CartesianIndex) = getmask_at(m, Tuple(i))
+getmask_at(m::MaskIndexer, i::CartesianIndex) = getmask_at(m, Tuple(i))
 
-getmask_at(m::M, I...) where {M >: AbstractDatalessMask} = getmask_at(GetIndexer(m), I...)
-
+getmask_at(m::M, I::Tuple) where {M >: AbstractDatalessMask} = getmask_at(GetIndexer(m), I)
 
 
 ####################  Dataless Mask  ####################
 
 struct CausalMask <: AbstractDatalessMask end
 
-getmask_at(::CausalMask, i, j, _...) = j >= i
+getmask_at(::CausalMask, I::Tuple) = I[2] >= I[1]
 
 struct LocalMask <: AbstractDatalessMask
     width::Int
 end
 
-getmask_at(m::LocalMask, i, j, _...) = j - m.width < i < j + m.width
+getmask_at(m::LocalMask, I::Tuple) = I[2] - m.width < I[1] < I[2] + m.width
 
 struct RandomMask <: AbstractDatalessMask
     p::Float64
 end
 
-getmask_at(m::RandomMask, _::Integer...) = rand() > m.p
+getmask_at(m::RandomMask, _::Tuple) = rand() > m.p
 
 struct BandPartMask <: AbstractDatalessMask
     l::Int
@@ -58,7 +58,7 @@ end
 
 ####################  Array Mask  ####################
 
-getmask_at(m::BandPartMask, i, j, _...) = (m.l < 0 || i <= j + m.l) && (m.u < 0 || i >= j - m.u)
+getmask_at(m::BandPartMask, I::Tuple) = (m.l < 0 || I[1] <= I[2] + m.l) && (m.u < 0 || I[1] >= I[2] - m.u)
 
 struct SymLengthMask{N, L <: AbstractArray{Int32, N}} <: AbstractArrayMask
     len::L
@@ -73,8 +73,11 @@ adapt_structure(to, x::SymLengthMask) = SymLengthMask(adapt(to, x.len))
 
 Adapt.adapt(to::CUDA.Adaptor, m::AbstractArrayMask) = Indexer{typeof(m)}(map(Base.Fix1(Adapt.adapt, to), GetIndexer(m).__fields))
 
-Base.@propagate_inbounds function getmask_at(m::Indexer{<:SymLengthMask}, i, j, I...)
-    l = m.len[I...]
+# TODO: add boundcheck
+Base.@propagate_inbounds function getmask_at(m::Indexer{<:SymLengthMask}, I::Tuple)
+    i = I[1]
+    j = I[2]
+    l = m.len[Base.tail(Base.tail(I))...]
     return i <= l && j <= l
 end
 
@@ -87,9 +90,12 @@ BiLengthMask(q_len, k_len) = BiLengthMask(convert(AbstractArray{Int32}, q_len), 
 
 adapt_structure(to, x::BiLengthMask) = BiLengthMask(adapt(to, x.q_len), adapt(to, x.k_len))
 
-Base.@propagate_inbounds function getmask_at(m::Indexer{<:BiLengthMask}, i, j, I...)
-    ql = m.q_len[I...]
-    kl = m.k_len[I...]
+Base.@propagate_inbounds function getmask_at(m::Indexer{<:BiLengthMask}, I::Tuple)
+    i = I[1]
+    j = I[2]
+    J = Base.tail(Base.tail(I))
+    ql = m.q_len[J...]
+    kl = m.k_len[J...]
     return i <= kl && j <= ql
 end
 
@@ -107,7 +113,7 @@ GetIndexer(m::FlipMask) = Indexer{typeof(m)}((mask = GetIndexer(m.mask),))
 
 Adapt.adapt(to::CUDA.Adaptor, m::FlipMask) = Indexer{typeof(m)}((mask = adapt(to, m.mask),))
 
-Base.@propagate_inbounds getmask_at(m::Indexer{<:FlipMask}, i, j, I...) = !getmask_at(m.mask, i, j, I...)
+Base.@propagate_inbounds getmask_at(m::Indexer{<:FlipMask}, I::Tuple) = !getmask_at(m.mask, I)
 
 Base.show(io::IO, m::FlipMask) = (print(io, '!'); show(io, m.mask); io)
 
@@ -125,44 +131,26 @@ Base.:&(m1::CombinedMask{typeof(&)}, m2::AbstractAttenMask) = CombinedMask(&, (m
 Base.:&(m1::AbstractAttenMask, m2::CombinedMask{typeof(&)}) = CombinedMask(&, (m1, m2.masks...))
 Base.:&(m1::CombinedMask{typeof(&)}, m2::CombinedMask{typeof(&)}) = CombinedMask(&, (m1.masks..., m2.masks...))
 
-# Broadcast.broadcastable(m::CombinedMask) = Broadcast.broadcasted(m.f, map(Broadcast.broadcastable, m.masks)...)
-
 Adapt.adapt(to::CUDA.Adaptor, m::CombinedMask) = Indexer{typeof(m)}((f = adapt(to, m.f),
                                                                      masks = map(Base.Fix1(adapt, to), m.masks)))
-
 adapt_structure(to, x::CombinedMask) = CombinedMask(x.f, adapt(to, x.masks))
 GetIndexer(m::CombinedMask) = Indexer{typeof(m)}((m.f, masks = map(GetIndexer, m.masks)))
 
-
-# @inline Base.@propagate_inbounds _combine_getmask(f, masks::Tuple, I...) = f(getmask_at(masks[1], I...), _combine_getmask(f, Base.tail(masks), I...))
-# @inline Base.@propagate_inbounds _combine_getmask(f, masks::Tuple{<:Any, <:Any}, I...) = f(getmask_at(masks[1], I...), getmask_at(masks[2], I...))
-# @inline Base.@propagate_inbounds _combine_getmask(f, masks::Tuple{<:Any}, I...) = getmask_at(masks[1], I...)
-
-@inline function _combine_getmask(f, masks, I...)
+@inline function _combine_getmask(f, masks, I)
     if length(masks) == 2
-        m1 = getmask_at(masks[1], I...)
-        m2 = getmask_at(masks[2], I...)
+        m1 = getmask_at(masks[1], I)
+        m2 = getmask_at(masks[2], I)
         return f(m1, m2)
     else
-        m1 = getmask_at(masks[1], I...)
-        m2 = _combine_getmask(f, Base.tail(masks), I...)
+        m1 = getmask_at(masks[1], I)
+        m2 = _combine_getmask(f, Base.tail(masks), I)
         return f(m1, m2)
     end
 end
 
-Base.@propagate_inbounds function getmask_at(m::Indexer{M}, i, j, I...) where M <: CombinedMask
-    Base.@_inline_meta
-    return _combine_getmask(m.f, m.masks, i, j, I...)
-    # return broadcast(m.f, getmask_at.(m.masks, i, j, I...)...)
-    #return _combine_getmask(m.f, m.masks, (i, j, I...))
-    # r = getmask_at(m.masks[1], i, j, I...)
-    # for mask in Base.tail(m.masks)
-    #     @inbounds r = m.f(r, getmask_at(mask, i, j, I...))
-    # end
-    # return r
+Base.@propagate_inbounds function getmask_at(m::Indexer{M}, I::Tuple) where M <: CombinedMask
+    return _combine_getmask(m.f, m.masks, I)
 end
-
-
 
 function Base.show(io::IO, m::CombinedMask)
     print(io, '(')
@@ -175,11 +163,25 @@ function Base.show(io::IO, m::CombinedMask)
     io
 end
 
-struct BatchedMask{M<:AbstractArrayMask}
+struct BatchedMask{M<:AbstractArrayMask, S<:StaticInt} <: AbstractAttenMask
     mask::M
-    batch_dim::Int
+    batch_dim::S
 end
 
+BatchedMask(mask, batch_dim::Integer) = BatchedMask(mask, static(batch_dim))
+
+Adapt.adapt(to::CUDA.Adaptor, m::BatchedMask) = Indexer{typeof(m)}((mask = adapt(to, m.mask), batch_dim = m.batch_dim))
+
+adapt_structure(to, x::BatchedMask) = BatchedMask(adapt(to, x.mask), x.batch_dim)
+
+GetIndexer(m::BatchedMask) = Indexer{typeof(m)}((mask = GetIndexer(m.mask), batch_dim = m.batch_dim))
+
+Base.@propagate_inbounds function getmask_at(m::Indexer{M}, I::Tuple) where M <: BatchedMask
+    i = I[1]
+    j = I[2]
+    J = ntuple(i->I[i-1+m.batch_dim], 1+length(I)-m.batch_dim)
+    return getmask_at(m.mask, (i, j, J...))
+end
 
 """
 Trait-like type for holding operation related argument, defined how the mask should be apply to input array
