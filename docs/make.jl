69d3a5d0ef11f18fc269749996df896c804cde2a
chengchingwen <adgjl5645@hotmail.com>
2021-08-12 16:07:07 +0800

    add docs folder

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using NeuralAttentionlib
+using Documenter
+
+DocMeta.setdocmeta!(NeuralAttentionlib, :DocTestSetup, :(using NeuralAttentionlib); recursive=true)
+
+makedocs(;
+    modules=[NeuralAttentionlib],
+    authors="chengchingwen <adgjl5645@hotmail.com> and contributors",
+    repo="https://github.com/chengchingwen/NeuralAttentionlib.jl/blob/{commit}{path}#{line}",
+    sitename="NeuralAttentionlib.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://chengchingwen.github.io/NeuralAttentionlib.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "API Reference" => "api.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/chengchingwen/NeuralAttentionlib.jl",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,19 @@
+```@meta
+CurrentModule = NeuralAttentionlib
+```
+
+# [NeuralAttentionlib](https://github.com/chengchingwen/NeuralAttentionlib.jl)
+
+*Reusable functionality for defining custom attention/transformer layers.*
+
+
+## Outline
+
+```@contents
+Pages = [
+	"api.md",
+]
+```
+
+
+
