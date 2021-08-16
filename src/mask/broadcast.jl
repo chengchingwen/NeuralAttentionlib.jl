004b6f2b269417147a4c3314cb7a612c77c57ecd
chengchingwen <adgjl5645@hotmail.com>
2021-08-16 23:49:23 +0800

    fallback axes handler in broadcast

--- src/mask/broadcast.jl
+++ src/mask/broadcast.jl
@@ -1,4 +1,4 @@
-import Base.Broadcast: BroadcastStyle, Broadcasted, combine_axes
+import Base.Broadcast: BroadcastStyle, Broadcasted, combine_axes, check_broadcast_axes
 
 BroadcastStyle(::Type{<:AbstractAttenMask}) =  MaskStyle()
 
@@ -11,6 +11,12 @@ BroadcastStyle(a::MaskStyle{M}, b::Broadcast.BroadcastStyle) where M = MaskStyle
 
 Base.similar(bc::Broadcasted{MaskStyle{M}}, ::Type{Eltype}) where {M, Eltype} = similar(Broadcasted{M}(bc.f, bc.args, bc.axes), Eltype)
 
+
+"""
+    AxesConstrain
+
+Abstract type for define mask's constrain. return a tuple of concrete subtype of AxesConstrain.
+"""
 abstract type AxesConstrain end
 
 struct All1Constrain <: AxesConstrain
@@ -63,9 +69,16 @@ check_constrain(cs::Tuple{}, x) = x
 check_constrain(cs::Tuple, x) = check_constrain(Base.tail(cs), check_constrain(cs[1], x))
 
 check_constrain(m::AbstractAttenMask, x) = check_constrain(AxesConstrain(m), x)
+check_constrain(y, x) = Broadcast.broadcast_shape(axes(y), x) # fallback
 
 @inline combine_axes(A, B::AbstractAttenMask) = combine_axes(B, A)
 @inline combine_axes(A::AbstractAttenMask, B) = check_constrain(A, axes(B))
+@inline combine_axes(A, B::Broadcasted{<:MaskStyle}) = combine_axes(B, A)
+@inline combine_axes(A::Broadcasted{<:MaskStyle}, B) = check_constrain(A.args, axes(B))
+
+@inline check_broadcast_axes(shp, A::AbstractAttenMask) = check_constrain(A, shp)
+@inline check_broadcast_axes(shp, A::Broadcasted{<:MaskStyle}) = isnothing(A.axes) ? combine_axes(A.args, shp) : check_broadcast_axes(shp, axes(A))
+
 
 axislength(x::Integer) = x
 axislength(x) = length(x)
