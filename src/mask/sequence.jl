bebe24d27d0ef5232e49297622e053f24435ab2c
chengchingwen <adgjl5645@hotmail.com>
2022-10-11 06:13:36 +0800

    add reverse length mask

--- src/mask/sequence.jl
+++ src/mask/sequence.jl
@@ -32,7 +32,6 @@ LengthMask(len::AbstractArray) = LengthMask(convert(AbstractArray{Int32}, len))
 
 adapt_structure(to, x::LengthMask) = LengthMask(adapt(to, x.len))
 
-Base.@propagate_inbounds Base.getindex(m::Indexer{<:LengthMask}, _, j) = (l = m.len[]; j <= l)
 Base.@propagate_inbounds function Base.getindex(m::Indexer{<:LengthMask}, _, j, J::Integer...)
     l = m.len[J...]
     return j <= l
@@ -43,9 +42,42 @@ AxesConstraint(m::LengthMask{N}) where N = length(m.len) == 1 ? # only one mask
     (NDimConstraint(ndims(m)), ntuple(i->DimConstraint(i+2, size(m.len, i)), N)...)
 
 AttenMask(m::LengthMask) = SymLengthMask(m.len)
+AttenMask(q::LengthMask, k::LengthMask) = BiLengthMask(q.len, k.len)
 
 Base.:(+)(m::LengthMask, l::Integer) = LengthMask(m.len .+ l)
 Base.:(*)(m::LengthMask, l::Integer) = LengthMask(m.len .* l)
 Base.:(-)(m::LengthMask, l::Integer) = LengthMask(m.len .- l)
 Base.:(+)(l::Integer, m::LengthMask) = m + l
 Base.:(*)(l::Integer, m::LengthMask) = m * l
+
+struct RevLengthMask{N, L <: AbstractArray{Int32, N}} <: AbstractSequenceMask
+    len::L
+end
+
+Base.ndims(::RevLengthMask{N}) where N = N + 2
+
+require_dest(::RevLengthMask) = static(true)
+
+RevLengthMask(len::Integer) = RevLengthMask(Int32[len])
+RevLengthMask(len::AbstractArray) = RevLengthMask(convert(AbstractArray{Int32}, len))
+
+adapt_structure(to, x::RevLengthMask) = RevLengthMask(adapt(to, x.len))
+
+Base.@propagate_inbounds function Base.getindex(m::Indexer{<:RevLengthMask, <:Tuple}, _, j, J::Integer...)
+    cl = m.dest_size[2]
+    l = m.len[J...]
+    return cl - l < j
+end
+
+AxesConstraint(m::RevLengthMask{N}) where N = length(m.len) == 1 ? # only one mask
+    (NDimConstraint(2, true), All1Constraint(3, ndims(m))) :
+    (NDimConstraint(ndims(m)), ntuple(i->DimConstraint(i+2, size(m.len, i)), N)...)
+
+AttenMask(m::RevLengthMask) = RevSymLengthMask(m.len)
+AttenMask(q::RevLengthMask, k::RevLengthMask) = RevBiLengthMask(q.len, k.len)
+
+Base.:(+)(m::RevLengthMask, l::Integer) = RevLengthMask(m.len .+ l)
+Base.:(*)(m::RevLengthMask, l::Integer) = RevLengthMask(m.len .* l)
+Base.:(-)(m::RevLengthMask, l::Integer) = RevLengthMask(m.len .- l)
+Base.:(+)(l::Integer, m::RevLengthMask) = m + l
+Base.:(*)(l::Integer, m::RevLengthMask) = m * l
