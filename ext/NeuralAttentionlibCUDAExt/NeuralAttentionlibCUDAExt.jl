43025d54c48f05f7b4ea06eb577e83cc7e9c2e0b
chengchingwen <chengchingwen214@gmail.com>
2024-04-26 04:59:18 +0800

    remove patch noises

--- ext/NeuralAttentionlibCUDAExt/NeuralAttentionlibCUDAExt.jl
+++ ext/NeuralAttentionlibCUDAExt/NeuralAttentionlibCUDAExt.jl
@@ -13,10 +13,10 @@ using LinearAlgebra.BLAS: get_num_threads, set_num_threads
 
 const NAlib = NeuralAttentionlib
 
-GPUArraysCore.backend(T::Type{<:NAlib.CollapsedDimsArray{E,<:CuArray}}) where {E} = GPUArraysCore.backend(CuArray{E,3})
+GPUArraysCore.backend(T::Type{<:NAlib.CollapsedDimsArray{E, <:CuArray}}) where E = GPUArraysCore.backend(CuArray{E, 3})
 
-function NeuralAttentionlib.batched_transpose_f!(f, B::AnyGPUArray{T,3}, A::AnyGPUArray{T,3}) where {T}
-    axes(B, 1) == axes(A, 2) && axes(B, 2) == axes(A, 1) && axes(A, 3) == axes(B, 3) || throw(DimensionMismatch(string(f)))
+function NeuralAttentionlib.batched_transpose_f!(f, B::AnyGPUArray{T, 3}, A::AnyGPUArray{T, 3}) where T
+    axes(B,1) == axes(A,2) && axes(B,2) == axes(A,1) && axes(A,3) == axes(B,3) || throw(DimensionMismatch(string(f)))
     GPUArrays.gpu_call(B, A) do ctx, B, A
         idx = GPUArrays.@cartesianidx A
         @inbounds B[idx[2], idx[1], idx[3]] = f(A[idx[1], idx[2], idx[3]])
@@ -27,11 +27,11 @@ end
 
 import CUDA.CUBLAS
 for (fname, elty) in
-    ((:cublasDgemmStridedBatched, :Float64),
-    (:cublasSgemmStridedBatched, :Float32),
-    (:cublasHgemmStridedBatched, :Float16),
-    (:cublasZgemmStridedBatched, :ComplexF64),
-    (:cublasCgemmStridedBatched, :ComplexF32))
+    ((:cublasDgemmStridedBatched,:Float64),
+     (:cublasSgemmStridedBatched,:Float32),
+     (:cublasHgemmStridedBatched,:Float16),
+     (:cublasZgemmStridedBatched,:ComplexF64),
+     (:cublasCgemmStridedBatched,:ComplexF32))
     @eval begin
 
         @inline function NeuralAttentionlib.unsafe_gemm_strided_batched!(
@@ -42,10 +42,10 @@ for (fname, elty) in
             ptrC::CuPtr{$elty}, ldc::Int, strideC::Int, batchCount::Int)
 
             CUBLAS.$fname(CUBLAS.handle(),
-                transA, transB, m, n, k,
-                alpha, ptrA, lda, strideA,
-                ptrB, ldb, strideB, beta,
-                ptrC, ldc, strideC, batchCount)
+                          transA, transB, m, n, k,
+                          alpha, ptrA, lda, strideA,
+                          ptrB, ldb, strideB, beta,
+                          ptrC, ldc, strideC, batchCount)
             return nothing
         end
 
@@ -75,9 +75,9 @@ for (elty, array) in (
                         ptrCi = ptrC + (b - 1) * strideC * sizeof($elty)
 
                         NeuralAttentionlib.unsafe_gemm!(transA, transB, m, n, k,
-                            alpha, ptrAi, lda,
-                            ptrBi, ldb, beta,
-                            ptrCi, ldc)
+                                                        alpha, ptrAi, lda,
+                                                        ptrBi, ldb, beta,
+                                                        ptrCi, ldc)
                     end
                 end
                 set_num_threads(old_threads)
@@ -87,9 +87,9 @@ for (elty, array) in (
                     ptrBi = ptrB + (i - 1) * strideB * sizeof($elty)
                     ptrCi = ptrC + (i - 1) * strideC * sizeof($elty)
                     NeuralAttentionlib.unsafe_gemm!(transA, transB, m, n, k,
-                        alpha, ptrAi, lda,
-                        ptrBi, ldb, beta,
-                        ptrCi, ldc)
+                                                    alpha, ptrAi, lda,
+                                                    ptrBi, ldb, beta,
+                                                    ptrCi, ldc)
                 end
             end
             return nothing
@@ -117,9 +117,9 @@ for (elty, array) in (
 
         @inline function NeuralAttentionlib.gemm_strided_batched!(
             transA::Char, transB::Char,
-            alpha::($elty), A::$array{$elty,3},
-            B::$array{$elty,3}, beta::($elty),
-            C::$array{$elty,3})
+            alpha::($elty), A::$array{$elty, 3},
+            B::$array{$elty, 3}, beta::($elty),
+            C::$array{$elty, 3})
 
             Base.require_one_based_indexing(A, B, C)
             BLAS.chkstride1(A, B, C)
@@ -131,13 +131,13 @@ for (elty, array) in (
             kb = size(B, transB == 'N' ? 1 : 2)
             n = size(B, transB == 'N' ? 2 : 1)
 
-            if m != size(C, 1) || n != size(C, 2) || ka != kb
+            if m != size(C,1) || n != size(C,2) || ka != kb
                 throw(DimensionMismatch("A has size ($m,$ka,$(size(A, 3))), B has size ($kb,$n,$(size(B, 3))), C has size $(size(C))"))
             end
 
-            lda = max(1, stride(A, 2))
-            ldb = max(1, stride(B, 2))
-            ldc = max(1, stride(C, 2))
+            lda = max(1, stride(A,2))
+            ldb = max(1, stride(B,2))
+            ldc = max(1, stride(C,2))
 
             strideA = size(A, 3) == 1 ? 0 : stride(A, 3)
             strideB = size(B, 3) == 1 ? 0 : stride(B, 3)
@@ -155,25 +155,25 @@ for (elty, array) in (
 
         function NeuralAttentionlib.gemm_strided_batched(
             transA::Char, transB::Char,
-            alpha::($elty), A::$array{$elty,3},
-            B::$array{$elty,3})
+            alpha::($elty), A::$array{$elty, 3},
+            B::$array{$elty, 3})
             C = similar(B, (size(A, transA == 'N' ? 1 : 2), size(B, transB == 'N' ? 2 : 1), max(size(A, 3), size(B, 3))))
             return NeuralAttentionlib.gemm_strided_batched!(transA, transB, alpha, A, B, zero($elty), C)
         end
 
         function NeuralAttentionlib.gemm_strided_batched(
             transA::Char, transB::Char,
-            A::$array{$elty,3},
-            B::$array{$elty,3})
+            A::$array{$elty, 3},
+            B::$array{$elty, 3})
             return NeuralAttentionlib.gemm_strided_batched(transA, transB, one($elty), A, B)
         end
 
         function NeuralAttentionlib.gemm_strided_batched!(
             transA::Char, transB::Char,
-            alpha::($elty), A::$array{$elty,N1},
-            B::$array{$elty,N2}, beta::($elty),
-            C::$array{$elty,N3},
-            Ai, Aj, Bi, Bj, Ci, Cj) where {N1,N2,N3}
+            alpha::($elty), A::$array{$elty, N1},
+            B::$array{$elty, N2}, beta::($elty),
+            C::$array{$elty, N3},
+            Ai, Aj, Bi, Bj, Ci, Cj) where {N1, N2, N3}
 
             # (a1, a2, ..., ai-1, ai, ai+1, ..., aj-1, aj, ..., an)
             #  |______lda______|  |____K/M (Ai)_____|  |___Aj____|
@@ -221,15 +221,15 @@ for (elty, array) in (
 
         function NeuralAttentionlib.gemm_strided_batched(
             transA::Char, transB::Char,
-            alpha::($elty), A::$array{$elty,N1},
-            B::$array{$elty,N2},
-            Ai, Aj, Bi, Bj) where {N1,N2}
+            alpha::($elty), A::$array{$elty, N1},
+            B::$array{$elty, N2},
+            Ai, Aj, Bi, Bj) where {N1, N2}
 
             m = NeuralAttentionlib.noncollapsed_size(A, Ai, Aj, transA == 'N' ? 1 : 2)
             n = NeuralAttentionlib.noncollapsed_size(B, Bi, Bj, transB == 'N' ? 2 : 1)
             sc3 = NeuralAttentionlib.collapsed_size(A, Ai, Aj, 3) > NeuralAttentionlib.collapsed_size(B, Bi, Bj, 3) ?
-                  NeuralAttentionlib.noncollapsed_size(A, Ai, Aj, 3) :
-                  NeuralAttentionlib.noncollapsed_size(B, Bi, Bj, 3)
+                NeuralAttentionlib.noncollapsed_size(A, Ai, Aj, 3) :
+                NeuralAttentionlib.noncollapsed_size(B, Bi, Bj, 3)
 
             Ci = length(n)
             Cj = length(sc3)
@@ -240,9 +240,9 @@ for (elty, array) in (
 
         function NeuralAttentionlib.gemm_strided_batched(
             transA::Char, transB::Char,
-            A::$array{$elty,N1},
-            B::$array{$elty,N2},
-            Ai, Aj, Bi, Bj) where {N1,N2}
+            A::$array{$elty, N1},
+            B::$array{$elty, N2},
+            Ai, Aj, Bi, Bj) where {N1, N2}
             return NeuralAttentionlib.gemm_strided_batched(transA, transB, one($elty), A, B, Ai, Aj, Bi, Bj)
         end
 
@@ -253,15 +253,13 @@ NeuralAttentionlib.check_strided_gemm_type(A::CuArray{Float16}) = true
 
 Adapt.adapt(to::CUDA.KernelAdaptor, m::AbstractArrayMask) =
     Indexer{typeof(m)}(map(Base.Fix1(Adapt.adapt, to), GetIndexer(m).__fields))
-Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.FlipMask) = Indexer{typeof(m)}((mask=adapt(to, m.mask),))
+Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.FlipMask) = Indexer{typeof(m)}((mask = adapt(to, m.mask),))
 Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.CombinedMask) =
-    Indexer{typeof(m)}((f=adapt(to, m.f), masks=map(Base.Fix1(adapt, to), m.masks)))
+    Indexer{typeof(m)}((f = adapt(to, m.f), masks = map(Base.Fix1(adapt, to), m.masks)))
 Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.BatchedMask) =
-    Indexer{typeof(m)}((mask=adapt(to, m.mask), batch_dim=static(m.batch_dim)))
-Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.RepeatMask) = Indexer{typeof(m)}((mask=adapt(to, m.mask), num=m.num))
+    Indexer{typeof(m)}((mask = adapt(to, m.mask), batch_dim = static(m.batch_dim)))
+Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.RepeatMask) = Indexer{typeof(m)}((mask = adapt(to, m.mask), num = m.num))
 Adapt.adapt(to::CUDA.KernelAdaptor, m::NAlib.BiSequenceMask) =
-    Indexer{typeof(m)}((q_mask=adapt(to, m.q_mask), k_mask=adapt(to, m.k_mask)))
+    Indexer{typeof(m)}((q_mask = adapt(to, m.q_mask), k_mask = adapt(to, m.k_mask)))
 
 end
-
-
--- test/functional.jl
+++ test/functional.jl
@@ -8,18 +8,18 @@
     using NeuralAttentionlib.Matmul
     using NeuralAttentionlib.Functional
     using NeuralAttentionlib: as_collapsed,
-        get_scalar_relative_position_embeddings,
-        t5_bucketed_position_id, t5_causal_bucketed_position_id,
-        layer_norm, rms_layer_norm, get_sincos_position_embeddings
+      get_scalar_relative_position_embeddings,
+      t5_bucketed_position_id, t5_causal_bucketed_position_id,
+      layer_norm, rms_layer_norm, get_sincos_position_embeddings
 
     @testset "score" begin
-        if !USE_CUDA && !USE_AMDGPU
+        if !USE_CUDA
             @testset "AD" begin
-                test_rrule(dot_product_score, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(dot_product_score, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(dot_product_score, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(scaled_dot_product_score, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(scaled_dot_product_score, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, randn(5, 3, 2), randn(5, 4, 2), 0.5; check_inferred=false)
+                test_rrule(scaled_dot_product_score, randn(5, 3, 2), randn(5, 4, 2), 0.5; check_inferred = false)
                 test_rrule(scaled_dot_product_score, randn(5, 3, 2, 2), randn(5, 4, 2, 2), 0.5)
 
                 f2(x) = 2 .* x
@@ -34,23 +34,23 @@
                     pullback(Ȳ) = (NoTangent(), 3 .* unthunk(Ȳ))
                     return y, pullback
                 end
-                test_rrule(dot_product_score, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(dot_product_score, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(dot_product_score, f2, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(dot_product_score, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(dot_product_score, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(dot_product_score, f2, f3, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(scaled_dot_product_score, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(scaled_dot_product_score, f2, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(scaled_dot_product_score, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(scaled_dot_product_score, f2, f3, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, 0.5, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(scaled_dot_product_score, 0.5, f2, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(scaled_dot_product_score, 0.5, f2, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
-                test_rrule(scaled_dot_product_score, 0.5, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred=false)
+                test_rrule(scaled_dot_product_score, 0.5, f2, f3, randn(5, 3, 2), randn(5, 4, 2); check_inferred = false)
                 test_rrule(scaled_dot_product_score, 0.5, f2, f3, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
 
                 test_rrule(biased_score, randn(4, 3), dot_product_score, randn(5, 3, 2), randn(5, 4, 2);
-                    check_inferred=false)
+                           check_inferred = false)
                 test_rrule(biased_score, randn(4, 3, 2), dot_product_score, randn(5, 3, 2), randn(5, 4, 2);
-                    check_inferred=false)
+                           check_inferred = false)
                 test_rrule(biased_score, randn(4, 3), dot_product_score, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
                 test_rrule(biased_score, randn(4, 3, 2), dot_product_score, randn(5, 3, 2, 2), randn(5, 4, 2, 2))
                 test_rrule(
@@ -58,13 +58,13 @@
                     dot_product_score,
                     CollapsedDimsArray(randn(5, 21, 3, 2), 1, 2),
                     CollapsedDimsArray(randn(5, 21, 3, 2), 1, 2),
-                    ; check_inferred=false)
+                    ; check_inferred = false)
                 test_rrule(
-                    normalized_score, x -> (x .^ 2) ./ sum(x; dims=1),
+                    normalized_score, x-> (x .^ 2) ./ sum(x; dims=1) ,
                     dot_product_score,
                     CollapsedDimsArray(randn(5, 21, 3, 2), 1, 2),
                     CollapsedDimsArray(randn(5, 21, 3, 2), 1, 2),
-                    ; check_inferred=false)
+                    ; check_inferred = false)
             end
         end
     end
@@ -84,7 +84,7 @@
             end
         end
 
-        if !USE_CUDA && !USE_AMDGPU
+        if !USE_CUDA
             @testset "AD" begin
                 test_rrule(
                     scalar_relative_position_embedding, t5_bucketed_position_id(8, 20), randn(3, 8),
@@ -133,15 +133,15 @@
     @testset "sincos_position_embeddings" begin
         function PE(size, pos, i::Int)
             if rem(i, 2) == 1
-                sin((pos - 1) / 1e4^((i - 1) / size))
+                sin((pos-1)/1e4^((i-1)/size))
             else
-                cos((pos - 1) / 1e4^((i - 2) / size))
+                cos((pos-1)/1e4^((i-2)/size))
             end
         end
         function sincos_pe(size, max_len)
             embedding = Matrix{Float32}(undef, size, max_len)
             for l = 1:max_len
-                map!(i -> PE(size, l, i), selectdim(embedding, 2, l), 1:size)
+                map!(i->PE(size, l, i), selectdim(embedding, 2, l), 1:size)
             end
             return embedding
         end
@@ -165,8 +165,8 @@
     @testset "rotary position embedding" begin
         function naive_rotary_pe(x)
             sincos = get_sincos_position_embeddings(size(x, 1), false, x)
-            sinθ = repeat(sincos[1:2:end, :], inner=(2, 1))
-            cosθ = repeat(sincos[2:2:end, :], inner=(2, 1))
+            sinθ = repeat(sincos[1:2:end, :], inner = (2, 1))
+            cosθ = repeat(sincos[2:2:end, :], inner = (2, 1))
             x1 = reshape(x[1:2:end, :, :, :], 1, size(x, 1) >> 1, Base.tail(size(x))...)
             x2 = reshape(x[2:2:end, :, :, :], 1, size(x, 1) >> 1, Base.tail(size(x))...)
             y = reshape(vcat(-x2, x1), size(x))
@@ -182,23 +182,23 @@
         @test with_rotary_position_embedding(x) ≈ naive_rotary_pe(x)
         @test with_rotary_position_embedding(256, x) ≈ naive_rotary_pe_w_dim(256, x)
         @test with_rotary_position_embedding(256)(x) ≈ naive_rotary_pe_w_dim(256, x)
-        if !USE_CUDA && !USE_AMDGPU
+        if !USE_CUDA
             @testset "AD" begin
                 x = randn(512, 5, 3, 2)
-                @test Zygote.gradient(x -> sum(sin.(with_rotary_position_embedding(x))), x)[1] ≈
-                      Zygote.gradient(x -> sum(sin.(naive_rotary_pe(x))), x)[1]
-                @test Zygote.gradient(x -> sum(sin.(with_rotary_position_embedding(256, x))), x)[1] ≈
-                      Zygote.gradient(x -> sum(sin.(naive_rotary_pe_w_dim(256, x))), x)[1]
-                @test Zygote.gradient(x -> sum(sin.(with_rotary_position_embedding(256)(x))), x)[1] ≈
-                      Zygote.gradient(x -> sum(sin.(naive_rotary_pe_w_dim(256, x))), x)[1]
+                @test Zygote.gradient(x->sum(sin.(with_rotary_position_embedding(x))), x)[1] ≈
+                    Zygote.gradient(x->sum(sin.(naive_rotary_pe(x))), x)[1]
+                @test Zygote.gradient(x->sum(sin.(with_rotary_position_embedding(256, x))), x)[1] ≈
+                    Zygote.gradient(x->sum(sin.(naive_rotary_pe_w_dim(256, x))), x)[1]
+                @test Zygote.gradient(x->sum(sin.(with_rotary_position_embedding(256)(x))), x)[1] ≈
+                    Zygote.gradient(x->sum(sin.(naive_rotary_pe_w_dim(256, x))), x)[1]
                 x = randn(512, 5, 2)
                 @test Zygote.gradient(
-                    x -> sum(sin.(scaled_dot_product_score(with_rotary_position_embedding(256), x, x))), x)[1] ≈
-                      Zygote.gradient(
-                    x -> sum(sin.(scaled_dot_product_score(Base.Fix1(naive_rotary_pe_w_dim, 256), x, x))), x)[1]
+                    x->sum(sin.(scaled_dot_product_score(with_rotary_position_embedding(256), x, x))), x)[1] ≈
+                Zygote.gradient(
+                    x->sum(sin.(scaled_dot_product_score(Base.Fix1(naive_rotary_pe_w_dim, 256), x, x))), x)[1]
                 test_rrule(
                     scaled_dot_product_score, with_rotary_position_embedding, randn(512, 5, 2), randn(512, 5, 2);
-                    check_inferred=false)
+                    check_inferred = false)
             end
         end
     end
@@ -218,38 +218,38 @@
         @test LN0(x) ≈ layer_norm(0, LN0.diag.scale, LN0.diag.bias, x)
         @test rms_layer_norm_naive(LN0.diag.scale, x) ≈ rms_layer_norm(0, LN0.diag.scale, x)
 
-        x32 = drand(Float32, 100, 2) .+ 1.0f4
-        @test isapprox(Flux.normalise(x32; dims=1, ϵ=1.0f-10), layer_norm(1.0f-10, nothing, nothing, x32); atol=2e-1)
+        x32 = drand(Float32, 100, 2) .+ 1f4
+        @test isapprox(Flux.normalise(x32; dims=1, ϵ = 1f-10), layer_norm(1f-10, nothing, nothing, x32); atol = 2e-1)
         @test isapprox(
-            Flux.gradient(x -> sum(sin.(Flux.normalise(x; dims=1, ϵ=1.0f-10))), x32)[1],
-            Flux.gradient(x -> sum(sin.(layer_norm(1.0f-10, nothing, nothing, x))), x32)[1];
-            atol=5e-1
+            Flux.gradient(x->sum(sin.(Flux.normalise(x; dims=1, ϵ = 1f-10))), x32)[1],
+            Flux.gradient(x->sum(sin.(layer_norm(1f-10, nothing, nothing, x))), x32)[1];
+            atol = 5e-1
         )
 
-        if !USE_CUDA && !USE_AMDGPU
+        if !USE_CUDA
             @testset "AD" begin
                 g = randn(20)
                 b = randn(20)
                 x = randn(20, 7)
-                test_rrule(layer_norm, 1e-5, g, b, x; atol=1e-5)
-                test_rrule(rms_layer_norm, 1e-5, g, x; atol=1e-5, check_inferred=false)
-                test_rrule(layer_norm, g, b, x; atol=1e-5)
-                test_rrule(rms_layer_norm, g, x; atol=1e-5, check_inferred=false)
+                test_rrule(layer_norm, 1e-5, g, b, x; atol = 1e-5)
+                test_rrule(rms_layer_norm, 1e-5, g, x; atol = 1e-5, check_inferred = false)
+                test_rrule(layer_norm, g, b, x; atol = 1e-5)
+                test_rrule(rms_layer_norm, g, x; atol = 1e-5, check_inferred = false)
                 test_rrule(layer_norm, 0, g, b, x)
-                test_rrule(rms_layer_norm, 0, g, x; check_inferred=false)
+                test_rrule(rms_layer_norm, 0, g, x; check_inferred = false)
 
-                test_rrule(layer_norm, 1e-5, 0.5, 0.3, x; atol=1e-5)
-                test_rrule(rms_layer_norm, 1e-5, 0.5, x; atol=1e-5, check_inferred=false)
-                test_rrule(layer_norm, 0.5, 0.3, x; atol=1e-5)
-                test_rrule(rms_layer_norm, 0.5, x; atol=1e-5, check_inferred=false)
+                test_rrule(layer_norm, 1e-5, 0.5, 0.3, x; atol = 1e-5)
+                test_rrule(rms_layer_norm, 1e-5, 0.5, x; atol = 1e-5, check_inferred = false)
+                test_rrule(layer_norm, 0.5, 0.3, x; atol = 1e-5)
+                test_rrule(rms_layer_norm, 0.5, x; atol = 1e-5, check_inferred = false)
                 test_rrule(layer_norm, 0, 0.5, 0.3, x)
-                test_rrule(rms_layer_norm, 0, 0.5, x; check_inferred=false)
+                test_rrule(rms_layer_norm, 0, 0.5, x; check_inferred = false)
 
-                test_rrule(layer_norm, 1e-5, 0.5, b, x; atol=1e-5)
-                test_rrule(layer_norm, 0.5, b, x; atol=1e-5)
+                test_rrule(layer_norm, 1e-5, 0.5, b, x; atol = 1e-5)
+                test_rrule(layer_norm, 0.5, b, x; atol = 1e-5)
                 test_rrule(layer_norm, 0, 0.5, b, x)
-                test_rrule(layer_norm, 1e-5, g, 0.3, x; atol=1e-5)
-                test_rrule(layer_norm, g, 0.3, x; atol=1e-5)
+                test_rrule(layer_norm, 1e-5, g, 0.3, x; atol = 1e-5)
+                test_rrule(layer_norm, g, 0.3, x; atol = 1e-5)
                 test_rrule(layer_norm, 0, g, 0.3, x)
 
             end
@@ -258,13 +258,13 @@
 
     @testset "attention" begin
         @testset "multihead_qkv_attention" begin
-            if !USE_CUDA && !USE_AMDGPU
+            if !USE_CUDA
                 @testset "AD" begin
                     for i = 1:3
                         a = randn(20, 3, 2)
                         b = randn(20, 5, 2)
                         c = randn(20, 5, 2)
-                        test_rrule(multihead_qkv_attention, 4, a, b, c; atol=1e-5)
+                        test_rrule(multihead_qkv_attention, 4, a, b, c; atol = 1e-5)
                     end
                 end
             end
@@ -290,19 +290,19 @@
             b = drandn(10, 5, 2)
             c = drandn(10, 5, 2)
 
-            grad = Zygote.gradient((x, y, z) -> sum(sin.(grouped_query_attention(6, 2, x, y, z))), a, b, c)
-            ngrad = Zygote.gradient((x, y, z) -> sum(sin.(naive_grouped_query_attention(6, 2, x, y, z))), a, b, c)
+            grad = Zygote.gradient((x,y,z)->sum(sin.(grouped_query_attention(6, 2, x, y, z))), a, b, c)
+            ngrad = Zygote.gradient((x,y,z)->sum(sin.(naive_grouped_query_attention(6, 2, x, y, z))), a, b, c)
             @test grad[1] ≈ ngrad[1]
             @test grad[2] ≈ ngrad[2]
             @test grad[3] ≈ ngrad[3]
 
-            if !USE_CUDA && !USE_AMDGPU
+            if !USE_CUDA
                 @testset "AD" begin
                     for i = 1:3
                         a = randn(30, 3, 2)
                         b = randn(10, 5, 2)
                         c = randn(10, 5, 2)
-                        test_rrule(grouped_query_attention, 6, 2, a, b, c; atol=1e-5)
+                        test_rrule(grouped_query_attention, 6, 2, a, b, c; atol = 1e-5)
                     end
                 end
             end
